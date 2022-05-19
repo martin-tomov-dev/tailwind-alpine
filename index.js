78dce5ff22af4a30b6c661def8f56f241ccf4c1d
martin-tomov-dev <devsonspree@gmail.com>
2022-05-19 17:34:28 +0100

    no message

--- index.js
+++ index.js
@@ -8,10 +8,19 @@ function form() {
 return {
     firstName: '',
     lastName: '',
+    destination: '',
     hand: '👋',
+    name_disabled: true,
+    dest_disabled: true,
     init() {
         this.firstName = sessionStorage.getItem("firstName");
         this.lastName = sessionStorage.getItem("lastName");
+        if (this.firstName != '' && this.firstName != null && this.lastName != '' && this.lastName != null) {
+            this.name_disabled = false;
+        }
+        if (this.destination != '' && this.dest_disabled != null) {
+            this.dest_disabled = false;
+        }
     },
     submit: function (event) {
         sessionStorage.setItem("firstName", this.firstName);
@@ -23,6 +32,17 @@ return {
         }
         else {
             event.target.parentElement.style.borderColor = 'rgb(209 213 219)';
+        }
+        if (this.firstName != '' && this.lastName != '' && this.firstName != null && this.lastName != null) {
+            this.name_disabled = false;
+        }
+        else {
+            this.name_disabled = true;
+        }
+        if (this.destination != '' && this.destination != null) {
+            this.dest_disabled = false;
+        } else {
+            this.dest_disabled = true;
         }
     }
 };
